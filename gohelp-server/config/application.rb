58f75e88ae8ff33b531adf90e382bc4f3af2805f
ahmadfahmip <afahmi3@yahoo.co.id>
2018-08-04 09:02:39 +0700

    Configure to API app

--- gohelp-server/config/application.rb
+++ gohelp-server/config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module GohelpServer
   class Application < Rails::Application
+    # API only app
+    config.api_only = true
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
